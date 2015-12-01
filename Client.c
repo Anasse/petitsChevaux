214f3476d5491b4670a7314c38a5bbadcdc128c9
CharliCho <charles-edouard.chomarat@e.ujf-grenoble.fr>
2015-12-01 16:54:25 +0100

    Refonte archi client + travaux en cours

--- Client.c
+++ Client.c
@@ -11,46 +11,193 @@
 #define MAX 256
 #define publicPort 7777
 
+void enumererChoixPossibles(int numSocket, int nbChevaux, int valeurDes);
+
 int main(int nbArgs, char* arg[]){
 	
-	int numPortPrive, numJoueur;
+	if(nbArgs != 3){
+		printf("Veuillez entrer 2 paramètres : - le nom du serveur\n - le numéro de port\n");
+		return EXIT_FAILURE;
+	}
 	
-	int arg2 = atoi(arg[2]);	
+	int numJoueur, nbJoueurs, nbChevaux, valeurDe;
+	int arg2 = atoi(arg[2]);
+	
+	/* On se connecte au socket public */
 	int numSocket = socketClient(arg[1], arg2, TCP);
 	if(numSocket == -1){
-		fprintf(stderr, "Erreur de création du socket !\n");
+		perror("Erreur de création du socket !\n");
 		return EXIT_FAILURE;
 	}
-	
-	printf("Connexion au socket public réussie\n");							//DEBUG
-	printf("J'attend mon numero de port\n");								//DEBUG
-	
-	read(numSocket, &numPortPrive, sizeof(int));
-	if(numPortPrive == 0){
-		printf("La partie est pleine ou déjà en cours !\n");
-		close(numSocket);
-		return EXIT_SUCCESS;
-	}
-	
-	printf("Je dois me connecter au port %d\n", numPortPrive);
-	
-	close(numSocket);
-	printf("Socket public fermé\n");										//DEBUG
-	
-	numSocket = socketClient(arg[1], numPortPrive, TCP);
-	if(numSocket == -1){
-		fprintf(stderr, "Erreur de création du socket !\n");
-		return EXIT_FAILURE;
-	}
-	
-	printf("Connexion au socket privé réussie\n");							//DEBUG
-	
+		
+	/* On lit notre numero de joueur */
 	read(numSocket, &numJoueur, sizeof(int));
 	printf("Mon numéro de joueur est %d\n", numJoueur);
 	
-	int signalDepart;
-	read(numSocket, &signalDepart, sizeof(int));
-	printf("signal : %d\n", signalDepart);
+	/* On lit le nombre de joueurs */
+	read(numSocket, &nbJoueurs, sizeof(int));
+	printf("Il y a %d joueurs\n", nbJoueurs);
 	
+	/* On lit le nombre de chevaux */
+	read(numSocket, &nbChevaux, sizeof(int));
+	printf("Il y a %d chevaux par joueur\n", nbChevaux);
+	
+	int signalDepart;
+		
+	/* On attend et on lit le signal de depart */
+	read(numSocket, &signalDepart, sizeof(int));
+	printf("C'est parti ! (signal : %d)\n", signalDepart);
+	
+	//int jouer = 1;
+	//while(jouer){
+		///* On lit le num du joueur du tour */
+		//int joueurDuTour;
+		//read(numSocket, &joueurDuTour, sizeof(int));
+		//printf("C'est le tour du joueur numéro %d\n", joueurDuTour);
+		
+		//if(joueurDuTour != numJoueur){
+			//printf("C'est votre tour");
+		//}
+		
+		///* On lit la valeur du de */
+		//read(numSocket, &valeurDe, sizeof(int));
+		//printf("La valeur du dé est : %d\n", valeurDe);
+		
+		////Lire les données du tour
+				
+		//if(joueurDuTour != numJoueur){
+			//// Jouer son tour
+			
+			//enumererChoixPossibles(numSocket, nbChevaux, valeurDe);
+			//scanf(%d
+		//}
+	//}
+	
+	close(numSocket);
 	return EXIT_SUCCESS;
 }
+
+void enumererChoixPossibles(int numSocket, int nbChevaux, int valeurDe){
+	printf("Veuillez choisir un mouvement : \n");
+	if(nbChevaux == 2){
+		char choix[8];
+		read(numSocket,choix,8);
+		if(choix[0]){
+			printf("0 - Sortir un cheval 1\n");
+		}
+		if(choix[1]){
+			printf("1 - Sortir un cheval 2\n");
+		}
+		if(choix[4]){
+			printf("4 - Avancer le cheval 1 de %d case(s)\n", valeurDe);
+		}
+		if(choix[5]){
+			printf("5 - Avancer le cheval 2 de %d case(s)\n", valeurDe);
+		}
+		if(choix[8]){
+			printf("8 - Monter l'escalier avec le cheval 1\n");
+		}
+		if(choix[9]){
+			printf("9 - Monter l'escalier avec le cheval 2\n");
+		}
+		if(choix[12]){
+			printf("12 - Faire gagner le cheval 1\n");
+		}
+		if(choix[13]){
+			printf("13 - Faire gagner le cheval 2\n");
+		}
+	}
+	else if(nbChevaux == 3){
+		char choix[12];
+		read(numSocket,choix,12);
+		if(choix[0]){
+			printf("0 - Sortir un cheval 1\n");
+		}
+		if(choix[1]){
+			printf("1 - Sortir un cheval 2\n");
+		}
+		if(choix[2]){
+			printf("2 - Sortir un cheval 3\n");
+		}
+		if(choix[4]){
+			printf("4 - Avancer le cheval 1 de %d case(s)\n", valeurDe);
+		}
+		if(choix[5]){
+			printf("5 - Avancer le cheval 2 de %d case(s)\n", valeurDe);
+		}
+		if(choix[6]){
+			printf("6 - Avancer le cheval 3 de %d case(s)\n", valeurDe);
+		}
+		if(choix[8]){
+			printf("8 - Monter l'escalier avec le cheval 1\n");
+		}
+		if(choix[9]){
+			printf("9 - Monter l'escalier avec le cheval 2\n");
+		}
+		if(choix[10]){
+			printf("10 - Monter l'escalier avec le cheval 3\n");
+		}
+		if(choix[12]){
+			printf("12 - Faire gagner le cheval 1\n");
+		}
+		if(choix[13]){
+			printf("13 - Faire gagner le cheval 2\n");
+		}
+		if(choix[14]){
+			printf("14 - Faire gagner le cheval 3\n");
+		}
+	}
+	else if(nbChevaux == 4){
+		char choix[16];
+		read(numSocket,choix,16);
+		if(choix[0]){
+			printf("0 - Sortir un cheval 1\n");
+		}
+		if(choix[1]){
+			printf("1 - Sortir un cheval 2\n");
+		}
+		if(choix[2]){
+			printf("2 - Sortir un cheval 3\n");
+		}
+		if(choix[3]){
+			printf("3 - Sortir un cheval 4\n");
+		}
+		if(choix[4]){
+			printf("4 - Avancer le cheval 1 de %d case(s)\n", valeurDe);
+		}
+		if(choix[5]){
+			printf("5 - Avancer le cheval 2 de %d case(s)\n", valeurDe);
+		}
+		if(choix[6]){
+			printf("6 - Avancer le cheval 3 de %d case(s)\n", valeurDe);
+		}
+		if(choix[7]){
+			printf("7 - Avancer le cheval 4 de %d case(s)\n", valeurDe);
+		}
+		if(choix[8]){
+			printf("8 - Monter l'escalier avec le cheval 1\n");
+		}
+		if(choix[9]){
+			printf("9 - Monter l'escalier avec le cheval 2\n");
+		}
+		if(choix[10]){
+			printf("10 - Monter l'escalier avec le cheval 3\n");
+		}
+		if(choix[11]){
+			printf("11 - Monter l'escalier avec le cheval 4\n");
+		}
+		if(choix[12]){
+			printf("12 - Faire gagner le cheval 1\n");
+		}
+		if(choix[13]){
+			printf("13 - Faire gagner le cheval 2\n");
+		}
+		if(choix[14]){
+			printf("14 - Faire gagner le cheval 3\n");
+		}
+		if(choix[15]){
+			printf("15 - Faire gagner le cheval 4\n");
+		}
+	}
+}
+
