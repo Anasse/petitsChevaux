6cd6f8920e2241c134f5832fce20ef113bdb7463
CharliCho <charles-edouard.chomarat@e.ujf-grenoble.fr>
2015-12-01 11:21:56 +0100

    Partie client pour la connexion

---
+++ Client.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "Plateau.h"
+#include "GestionJeu.h"
+#include "Reseau.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#define MAX 256
+#define publicPort 7777
+
+int main(int nbArgs, char* arg[]){
+	
+	int numPortPrive, numJoueur;
+	
+	int arg2 = atoi(arg[2]);	
+	int numSocket = socketClient(arg[1], arg2, TCP);
+	if(numSocket == -1){
+		fprintf(stderr, "Erreur de création du socket !\n");
+		return EXIT_FAILURE;
+	}
+	
+	printf("Connexion au socket public réussie\n");							//DEBUG
+	printf("J'attend mon numero de port\n");								//DEBUG
+	
+	read(numSocket, &numPortPrive, sizeof(int));
+	if(numPortPrive == 0){
+		printf("La partie est pleine ou déjà en cours !\n");
+		close(numSocket);
+		return EXIT_SUCCESS;
+	}
+	
+	printf("Je dois me connecter au port %d\n", numPortPrive);
+	
+	close(numSocket);
+	printf("Socket public fermé\n");										//DEBUG
+	
+	numSocket = socketClient(arg[1], numPortPrive, TCP);
+	if(numSocket == -1){
+		fprintf(stderr, "Erreur de création du socket !\n");
+		return EXIT_FAILURE;
+	}
+	
+	printf("Connexion au socket privé réussie\n");							//DEBUG
+	
+	read(numSocket, &numJoueur, sizeof(int));
+	printf("Mon numéro de joueur est %d\n", numJoueur);
+		
+	return EXIT_SUCCESS;
+}
--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
 # Makefile pour le projet de jeu des petits chevaux en ligne
-# TODO : à compléter
 
 CC = gcc --std=c99 -Wall -g -c
 LD = gcc
 
-PROG=testPlateau coteClient coteServ
+PROG=testPlateau coteClient coteServ Client TestClient
 all: $(PROG)
 
 clean:
@@ -19,14 +18,11 @@ coteServ : coteServ.o Reseau.o
 testPlateau: testPlateau.o Plateau.o GestionJeu.o
 	$(LD) -o testPlateau testPlateau.o Plateau.o GestionJeu.o
 
-
-
 GestionJeu.o: GestionJeu.c GestionJeu.h
 	$(CC) GestionJeu.c
 
 Plateau.o: Plateau.c Plateau.h GestionJeu.h
 	$(CC) Plateau.c
-
 
 Reseau.o: Reseau.c Reseau.h
 	$(CC) Reseau.c 
@@ -36,3 +32,17 @@ coteClient.o : coteClient.c Reseau.h
 
 coteServ.o : coteServ.c Reseau.h
 	$(CC) coteServ.c
+
+Client : Client.o Reseau.o
+	$(LD) -o Client Client.o Reseau.o
+
+Client.o : Client.c Reseau.h
+	$(CC) Client.c
+
+TestClient : TestClient.o Reseau.o
+	$(LD) -o TestClient TestClient.o Reseau.o
+
+TestClient.o : TestClient.c Reseau.h
+	$(CC) TestClient.c
+
+	
