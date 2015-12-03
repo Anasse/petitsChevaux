07989fbfb2477938bd0b345dfb115fa7c7a803d9
Nono <noe.goudian@e.ujf-grenoble.fr>
2015-12-03 13:47:23 +0100

    Modif cosmétique, traces plus pertinentes coté serveur

--- Client.c
+++ Client.c
@@ -66,7 +66,7 @@ int main(int nbArgs, char* arg[]){
 		read(numSocket, pos, sizeof(int)*nbJoueurs*nbChevaux);
 		/*TRACE-DEBUG*/afftab(pos, nbJoueurs, nbChevaux);
 		/* On affiche le plateau de jeu */			
-		affichePlateau(5, 25, nbJoueurs, nbChevaux, pos);
+		affichePlateau(2, 22, nbJoueurs, nbChevaux, pos);
 		printf("***Tour %d --- C'est au joueur %d !\tRésultat du dé : %d\n", tour, joueurDuTour+1, valeurDe);
 			
 		if(joueurDuTour == numJoueur-1){
@@ -90,7 +90,7 @@ int main(int nbArgs, char* arg[]){
 			// Envoi du choix du joueur au serveur 
 			write(numSocket, &selectionChoix, sizeof(int));
 		}else{
-			printf("Tour des autres joueurs, merci de patienter.\n");
+			printf("Tour des autres joueurs, merci de patienter...\n");
 		}
 		/* Lecture du signal pour savoir si le jeu est fini */
 		read(numSocket, &signal, sizeof(int));
--- serveur.c
+++ serveur.c
@@ -71,7 +71,7 @@ int main (int nbArgs, char* args[]){
 		char *choixTemp = malloc(paramNbChevaux*4*sizeof(char));
 		char *choix = malloc(16*sizeof(char));
 		signal = 1;
-		while(tour < 50){
+		while(tour < 150){
 			int de = lancerDes();
 			//printf("Tour:%d  dé:%d  joueur:%d\n", tour, de, joueurDuTour);
 			i=0;
@@ -81,6 +81,7 @@ int main (int nbArgs, char* args[]){
 				write(tubes_interfaces[2*i+1][1], &joueurDuTour, sizeof(int));
 				write(tubes_interfaces[2*i+1][1], &de, sizeof(int));
 				write(tubes_interfaces[2*i+1][1], posChevaux, sizeof(int)*paramNbChevaux*paramNbJoueurs);
+				afftab(posChevaux, paramNbJoueurs, paramNbChevaux);
 				i++;
 			}
 			//APPEL possibilités
@@ -88,7 +89,7 @@ int main (int nbArgs, char* args[]){
 			etendreVecteurChoix(choixTemp, choix, paramNbChevaux); 
 			write(tubes_interfaces[2*joueurDuTour+1][1], choix, sizeof(char)*16);
 			read(tubes_interfaces[2*joueurDuTour][0], &selectionChoix, sizeof(int));
-			printf("Choix du client : %d\n", selectionChoix);
+			printf("Choix du client %d : %d\n", joueurDuTour+1, selectionChoix);
 			if(selectionChoix != 20){
 				//??appliquer
 				appliquerChoix(selectionChoix, joueurDuTour, de, posChevaux, paramNbChevaux);
@@ -130,7 +131,6 @@ int interface(int tubeIn, int tubeOut, int idParam, int sock, int nbJoueurs, int
 		read(tubeIn, &joueurDuTour, sizeof(int));
 		read(tubeIn, &de, sizeof(int));
 		read(tubeIn, pos, sizeof(int)*nbJoueurs*nbChevaux);
-		afftab(pos, nbJoueurs, nbChevaux);
 		write(sock, &signal, sizeof(int));	
 		write(sock, &tour, sizeof(int));
 		write(sock, &joueurDuTour, sizeof(int));
